19a2e6fc1bf26d997e365a30cae102423b38d811
Zaklekta Roman <romazak2006@gmail.com>
2023-11-06 16:44:38 +0200

    add functional that checks lines and create pilars

--- project.py
+++ project.py
@@ -7,3 +7,14 @@ def validate_board(board:list) -> bool:
 " 6  83  *","3      **","  8  2***","  2  ****"])
     False
     """
+    num=["1","2","3","4","5","6","7","8","9"]
+    pillars=['','','','','','','','','','']
+    color_panels=[]
+    for i in board:
+        y=0
+        while y<9:
+            pillars[y]+=i[y]
+            y+=1
+        for j in num:
+            if i.count(j)>1:
+                return False

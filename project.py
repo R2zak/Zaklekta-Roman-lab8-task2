29c40e2f507a9279f176434b5fa91f9610ee5322
Zaklekta Roman <romazak2006@gmail.com>
2023-11-06 16:40:59 +0200

    add base function

--- project.py
+++ project.py
@@ -1,1 +1,9 @@
-
+def validate_board(board:list) -> bool:
+    """
+    list->bool
+    Function that check game board and returns 
+    True or False depending on the fulfillment of the rules of the game
+    >>> validate_board(["**** ****","***1 ****","**  3****","* 4 1****","    19 5 ",\
+" 6  83  *","3      **","  8  2***","  2  ****"])
+    False
+    """
